c22ae813f85cb17ea7a5441948cb334567f1e133
Michael <michael.e@outlook.co.il>
2018-09-11 17:12:22 +0300

    form

---
+++ IOLayer/src/main/java/Input/MainGui.java
@@ -0,0 +1,64 @@
+package Input;
+
+import FilesLogic.FilesScanner;
+import Handlers.FilesContainer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class MainGui {
+    private JPanel panel1;
+    private JSpinner threadSpinner;
+    private JTextField pathField;
+    private JTextField totalFilesField;
+    private JTextField resultField;
+    private JButton checkPathButton;
+    private JButton scanButton;
+    private FilesScanner filesScanner;
+    public MainGui() {
+        checkPathButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String dir = pathField.getText();
+                if (new File(dir).exists()) {
+                    FileFetcher fileFetcher = new FileFetcher(dir);
+                    FilesContainer container = new FilesContainer();
+                    File[] files = fileFetcher.getFilesTree();
+                    totalFilesField.setText(String.valueOf(files.length));
+                    container.init(files);
+                    filesScanner = new FilesScanner(container.getFiles());
+                }
+                else {
+                    totalFilesField.setText("Bad Path");
+                }
+                totalFilesField.setDisabledTextColor(Color.BLACK);
+
+            }
+        });
+        scanButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (filesScanner != null){
+                    filesScanner.scan((Integer) threadSpinner.getValue());
+
+                    resultField.setText("Total time: "+filesScanner.getLastScanTime()+" | "+
+                            "Number Of Invalid Files: "+filesScanner.getState().getFalseFiles().size());
+                    resultField.setDisabledTextColor(Color.RED);
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainGui");
+        frame.setContentPane(new MainGui().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+}
